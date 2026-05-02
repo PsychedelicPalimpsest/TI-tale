7185b8f6edd2ab906f90b7d496773e75962c2a9c
HeronErin <HeronErin@protonmail.com>
2026-05-02 15:59:18 -0400

    Game loop scafold

---
+++ app/engine/engine.h
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+
+enum event_t{
+  ev_create,
+  ev_destroy,
+  ev_cleanup,
+
+  ev_step, ev_step_normal, ev_step_begin, ev_step_end,
+};
+
+
+
+
+typedef struct {
+} Object;
+
+typedef struct {
+} Instance;
+
+
+
+
---
+++ app/engine/gameloop.asm
@@ -0,0 +1,34 @@
+SECTION code_engine
+
+INCLUDE "core/common.inc"
+
+
+PUBLIC _game_loop
+_game_loop:
+
+  fheap_def globals_area, test, 64, 2
+  fheap_init test 
+
+
+
+@main_loop:
+; This is the low byte of the game counter
+  ld a, (_game_count)
+  push af
+
+
+
+
+
+; Wait until the low byte of the game counter changes (changed in interupt)
+  pop bc ; b = game counter
+@fps_limit_loop:
+  ld a, (_game_count)
+  cp b 
+  jp z, @fps_limit_loop
+  
+  jp @main_loop
+
+
+
+  ret
